abccdebec0eb899c1a63b0c43617c95bee2641b4
bkaiserinfosec <112435093+SecurityUniversal@users.noreply.github.com>
2023-07-15 09:13:07 -0700

    fix jenkins script error

--- src/main/java/io/jenkins/plugins/secusphere/SecuSphereSidePanel.java
+++ src/main/java/io/jenkins/plugins/secusphere/SecuSphereSidePanel.java
@@ -3,35 +3,33 @@ package io.jenkins.plugins.secusphere;
 import hudson.model.Action;
 import org.json.JSONObject;
 
-import java.io.IOException;
-
 public class SecuSphereSidePanel implements Action {
-    private final JSONObject result;
+    private final String resultString;
     private final String category;
 
     public SecuSphereSidePanel(JSONObject result, String category) {
-        this.result = result;
+        this.resultString = result.toString();
         this.category = category;
     }
 
     public JSONObject getResult() {
-        return result;
+        return new JSONObject(this.resultString);
     }
 
     public String getCriticalFindingCnt() {
-        return result.get("Critical").toString();
+        return getResult().get("Critical").toString();
     }
 
     public String getHighFindingCnt() {
-        return result.get("High").toString();
+        return getResult().get("High").toString();
     }
 
     public String getMediumFindingCnt() {
-        return result.get("Medium").toString();
+        return getResult().get("Medium").toString();
     }
 
     public String getLowFindingCnt() {
-        return result.get("Low").toString();
+        return getResult().get("Low").toString();
     }
 
     @Override
